d864b711e43bb9079a3521e2a9bb0fd960f1546c
edo98811 <edoardofilippi77@gmail.com>
2022-11-15 16:10:41 +0000

    modificato errore: - creata classe data_manipulation - creato file per convertire le immagini in nifty

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -9,49 +9,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import nibabel.freesurfer as fs
-import nibabel as nb
-import os
-import json
-from monai.bundle import run
-
-def read_annot(path, annotname):
-    return fs.read_annot(str(path + annotname))
-
-def read_img(path, imgname):
-    return nb.load(str(path + imgname))
-
-def load_data(all_files_list):
-    pass
-
-def list_files(dir):
-        r_img = []
-        r_annot = []
-        #r_all = []
-
-        for root, dirs, files in os.walk(dir):
-            #r_all.append(os.path.join(root))
-            for name in files:
-                l_name = name.split(".")
-                if len(l_name) < 2:
-                    continue
-                if l_name[-1] == "annot":
-                    r_annot.append(os.path.join(root, name))
-                elif l_name[-2] == "T1":
-                    r_img.append(os.path.join(root, name))
-
-        return {
-            #"r_all":r_all,
-            "r_img":r_img,
-            "r_annot":r_annot
-        }
-def write_file(all_files, filename):
-    json_object = json.dumps(all_files, indent=4)
-
-    with open(filename +".json", "w") as outfile:
-        outfile.write(json_object)
+import data_manipulation as dm
 
 if __name__=="__main__":
+    data_list = dm.load_dict("directory")
+
+    dm.convert_img(data_list["r_img"])
+
+"""
     basepath = "/media/neuropsycad/disk12t/VascoDiogo/OASIS/FS7/"
 
     all_files = list_files(basepath)
@@ -60,19 +25,6 @@ if __name__=="__main__":
     #run(runner_id=None, meta_file=None, config_file=None, logging_file=None, args_file=None)
     #run(None, meta_file="configs/metadata.json", config_file="configs/train.json", logging_file="configs/logging.conf", args_file=None)
 
-
-"""
-funzioni 
-
-read img
-read annot
-
-load all data (magari non serve nemmeno)
-
-load 
-
-
-prende solo i file che hanno le estensioni giuste (.annot)
 
 
 """
---
+++ scripts/data_manipulation.py
@@ -0,0 +1,60 @@
+
+
+import nibabel.freesurfer as fs
+import nibabel as nib
+import os
+import json
+from monai.bundle import run
+
+def read_annot(path, annotname):
+    return fs.read_annot(str(path + annotname))
+
+def read_img(path, imgname):
+    return nib.load(str(path + imgname))
+
+def list_files(dir):
+        r_img = []
+        r_annot = []
+        #r_all = []
+
+        for root, dirs, files in os.walk(dir):
+            #r_all.append(os.path.join(root))
+            for name in files:
+                l_name = name.split(".")
+                if len(l_name) < 2:
+                    continue
+                if l_name[-1] == "annot":
+                    r_annot.append(os.path.join(root, name))
+                elif l_name[-2] == "T1":
+                    r_img.append(os.path.join(root, name))
+
+        return {
+            #"r_all":r_all,
+            "r_img":r_img,
+            "r_annot":r_annot
+        }
+
+def write_dict(all_files, filename):
+    json_object = json.dumps(all_files, indent=4)
+
+    with open(filename +".json", "w") as outfile:
+        outfile.write(json_object)
+
+def load_dict(filename):
+    with open(filename +".json", "r") as outfile:
+        img_dict = json.load(outfile.load())
+
+    return img_dict
+
+def convert_img(img_list):
+    if not os.path.isdir("dataset"):
+        os.makedirs("dataset")
+
+    for img in img_list:
+
+        img = nib.load(img)
+
+        subj = img.split("/")[-2]
+        nib.save(img, f"dataset/{subj}_T1.nii.gz")
+
+

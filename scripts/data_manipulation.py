ec1e35801c4115d6d556e4a6f1b7d01b68fdbfb5
edo98811 <edoardofilippi77@gmail.com>
2022-11-15 16:25:39 +0000

    modificato errore: - directory :/

--- scripts/data_manipulation.py
+++ scripts/data_manipulation.py
@@ -47,14 +47,14 @@ def load_dict(filename):
     return img_dict
 
 def convert_img(img_list):
-    if not os.path.isdir("dataset"):
-        os.makedirs("dataset")
+    if not os.path.isdir("../dataset"):
+        os.makedirs("../dataset")
 
     for img_name in img_list:
 
         img = nib.load(img_name)
 
         subj = img_name.split("/")[-3]
-        nib.save(img, f"dataset/{subj}_T1.nii.gz")
+        nib.save(img, f"../dataset/{subj}_T1.nii.gz")
 
 

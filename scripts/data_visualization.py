6d54342bf8dddfcad3d0395ffe2d53194b89e85b
edo98811 <edoardofilippi77@gmail.com>
2022-11-17 16:29:46 +0000

    errore corretto

--- scripts/data_visualization.py
+++ scripts/data_visualization.py
@@ -6,7 +6,7 @@ def see_random_slice(img):
     fig = plt.figure(figsize=(9, 9))
     rows, cols = 4, 4
     for i in range(1, rows * cols + 1):
-        random_idx = torch.randint(0, img.shape[1], size=[1]).item() #transforms in normal python int
+        random_idx = torch.randint(0, img.shape[0], size=[1]).item() #transforms in normal python int
         img, label = img[random_idx,:,:]
         fig.add_subplot(rows, cols, i)
         plt.imshow(img.squeeze(), cmap="gray")
